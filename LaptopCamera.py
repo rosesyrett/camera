71851fa4f338b56484e6453f0bb340cdc46b7178
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2022-11-16 16:26:07 +0000

    working example emitting resource and datum documents

--- LaptopCamera.py
+++ LaptopCamera.py
@@ -3,18 +3,20 @@ import time
 from datetime import datetime
 from pathlib import Path
 from pprint import pprint
-from queue import Queue
-from typing import Dict
+from typing import Dict, Iterator, List
+import uuid
 
 import bluesky.plan_stubs as bps
 import bluesky.plans as bp
 from bluesky import RunEngine
-from bluesky.protocols import Descriptor, Reading, Status, SyncOrAsync
+from bluesky.protocols import Descriptor, Reading, Status, SyncOrAsync, Asset
 from ophyd import Component, Device, DeviceStatus, Signal
 
 from PhysicalCamera import VideoCaptureSignal
 
 import h5py
+import cv2
+from collections import deque
 
 os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
@@ -28,34 +30,91 @@ class LaptopCamera(Device):
     def __init__(self, **kwargs):
         super(LaptopCamera, self).__init__(**kwargs)
         self.stage_sigs[self.count] = 0
-        self.queue = Queue(1)
+        self._asset_docs_cache = deque()
+        self.filestore_spec = "HDF5"
+        self.path_semantics = "posix"
+        self.h5_file = None
+        self.resource_path = None
+
+    def resource_factory(
+        self,
+        resource_kwargs,
+    ):
+        resource_uid = str(uuid.uuid4())
+        resource_doc = {
+            "spec": self.filestore_spec,
+            "root": self.directory.get(),
+            "resource_path": str(self.resource_path),
+            "resource_kwargs": resource_kwargs,
+            "path_semantics": self.path_semantics,
+            "uid": resource_uid,
+        }
+
+        def datum_factory(datum_kwargs):
+            i = self.count.get()
+            datum_id = "{}/{}".format(resource_uid, i)
+            datum = {
+                "resource": resource_uid,
+                "datum_id": datum_id,
+                "datum_kwargs": datum_kwargs,
+            }
+
+            return datum
+
+        return resource_doc, datum_factory
+
+    def generate_resource(self, resource_kwargs) -> None:
+        # this generates a resource document
+
+        resource, self.datum_factory = self.resource_factory(
+            resource_kwargs,
+        )
+
+        self._asset_docs_cache.append(("resource", resource))
+
+    def generate_datum(self, datum_kwargs) -> str:
+        datum = self.datum_factory(datum_kwargs)
+        self._asset_docs_cache.append(("datum", datum))
+
+        datum_id = datum["datum_id"]
+        return datum_id
+
+    def stage(self) -> List["LaptopCamera"]:
+        self.resource_path = Path(self.filename.get() + ".h5py")
+
+        self.generate_resource({})
+
+        self.h5_file = h5py.File(
+            str(Path(self.directory.get()) / self.resource_path), "w"
+        )
+        return [self]
+
+    def unstage(self) -> List["LaptopCamera"]:
+        self.h5_file.close()
+        self._asset_docs_cache.clear()
+        cv2.destroyAllWindows()
+        return [self]
 
     def trigger(self) -> Status:
         image = self.camera.get()
         self.count.set(self.count.get() + 1)
-        path_to_data = "/%d" % self.count.get()
+        path_to_data = "%d" % self.count.get()
 
-        file_loc: Path = Path(self.directory.get()) / (self.filename.get() + ".h5py")
-        mode = "w" if not file_loc.is_file() else "r+"
+        self.h5_file.create_dataset(path_to_data, data=image)
 
-        with h5py.File(str(file_loc), mode) as h5_file:
-            h5_file.create_dataset(path_to_data, data=image)
+        cv2.imshow("video", image)
 
-        self.queue.put(path_to_data)
         status = DeviceStatus(self)
         status.set_finished()
         return status
 
     def read(self) -> SyncOrAsync[Dict[str, Reading]]:
-        if not self.queue.empty():
-            saved_location = self.queue.get_nowait()
-        else:
-            self.trigger()
-            self.read()
-
+        self.generate_datum({})
+        datum_id = self.datum_factory({})
         result = {
-            "location": Reading(
-                value=saved_location, timestamp=time.mktime(datetime.now().timetuple())
+            "value": Reading(
+                value=datum_id["datum_id"],
+                timestamp=time.mktime(datetime.now().timetuple()),
             )
         }
 
@@ -63,8 +122,14 @@ class LaptopCamera(Device):
 
     def describe(self) -> SyncOrAsync[Dict[str, Descriptor]]:
 
-        result = {"location": Descriptor(source="cv2 camera", dtype="string", shape=[])}
+        result = {"value": Descriptor(source="cv2 camera", dtype="string", shape=[])}
         return result
+
+    def collect_asset_docs(self) -> Iterator[Asset]:
+        items = list(self._asset_docs_cache)
+        self._asset_docs_cache.clear()
+        for item in items:
+            yield item
 
 
 lpc = LaptopCamera(name="lpc")
@@ -75,7 +140,7 @@ RE = RunEngine()
 def take_pictures():
     yield from bps.abs_set(lpc.directory, "/home/rose/Documents/projects/camera/webcam")
     yield from bps.abs_set(lpc.filename, "test")
-    yield from bp.count([lpc], num=10, delay=1)
+    yield from bp.count([lpc], num=10, delay=0.1)
 
 
 RE(take_pictures(), lambda name, doc: pprint({"name": name, "doc": doc}))
